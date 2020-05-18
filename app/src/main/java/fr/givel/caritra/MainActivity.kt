0fda14f7c2e841c1965620465c2757b265c9486e
Louis-Marie Givel <louis-marie@givel.fr>
2020-05-18 13:17:32 +0200

    remake + notification
     - Remake input interface
     - Add notification

--- app/src/main/java/fr/givel/caritra/MainActivity.kt
+++ app/src/main/java/fr/givel/caritra/MainActivity.kt
@@ -2,6 +2,9 @@ package fr.givel.caritra
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioGroup
+import com.google.android.material.snackbar.Snackbar
 import fr.givel.caritra.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +17,23 @@ class MainActivity : AppCompatActivity() {
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow()
         }
+    }
+
+    /**
+     * Shows a message when the form has been completed by the user
+     */
+
+    fun onFormCompleted(view: View){
+        // Find selected option
+        //var selected = findViewById(R.id.radioGroup)
+
+        // Notify it
+        Snackbar.make(
+            view,
+            getString(R.string.done),
+            Snackbar.LENGTH_SHORT
+        ).show()
+
+        //TODO: save in database
     }
 }
